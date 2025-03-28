ebab8d84c8decd18cf20d4f7a52dac29ebfe4445
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2025-03-28 12:09:29 -0700

    Add initial files

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// Changing the Text Content
+
+const appearanceHeading = document.getElementById('facts__heading');
+
+appearanceHeading.textContent = 'Qualities and Traits of a Crab';
+
+////////////////////////
+// Changing Appearance//
+////////////////////////
+
+// const importantFact = document.getElementById('facts__fact--important');
+
+// importantFact.className = `${importantFact.className} highlight`;
+
+///////////////////////////
+// Creating a New Element//
+///////////////////////////
+
+// const newAppearanceFact = document.createElement('li');
+
+// newAppearanceFact.textContent = 'covered in sand (when on a sandy beach)';
+
+// const appearanceList = document.getElementById('facts__list');
+
+// appearanceList.appendChild(newAppearanceFact);
