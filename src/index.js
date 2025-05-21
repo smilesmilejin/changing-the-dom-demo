f393235185414b6e9fc217496e8c66a10aa34476
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-21 01:22:17 -0400

    Changes based on Adding Behavior

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 // Changing the Text Content
 
+// Here, she selects the desired heading element by its id using document.getElementById(). 
 const appearanceHeading = document.getElementById('facts__heading');
 
+// Then, she sets its textContent attribute to 'Qualities and Traits of a Crab'.
 appearanceHeading.textContent = 'Qualities and Traits of a Crab';
 
 ////////////////////////
 // Changing Appearance//
 ////////////////////////
 
-// const importantFact = document.getElementById('facts__fact--important');
+// Then, she can select the important fact element and add this new class to the element:
+const importantFact = document.getElementById('facts__fact--important');
 
-// importantFact.className = `${importantFact.className} highlight`;
+// className is an attribute, not a method.
+// Note that she set importantFact.className to a string that interpolates the existing importantFact.className value! 
+importantFact.className = `${importantFact.className} highlight`;
 
 ///////////////////////////
 // Creating a New Element//
 ///////////////////////////
 
-// const newAppearanceFact = document.createElement('li');
+const newAppearanceFact = document.createElement('li');
 
-// newAppearanceFact.textContent = 'covered in sand (when on a sandy beach)';
+newAppearanceFact.textContent = 'covered in sand (when on a sandy beach)';
 
-// const appearanceList = document.getElementById('facts__list');
+const appearanceList = document.getElementById('facts__list');
 
-// appearanceList.appendChild(newAppearanceFact);
+appearanceList.appendChild(newAppearanceFact);
